376d27cfe6d447157af8d2a603b39b1b7fe73bbc
WangFan <whvcse@foxmail.com>
2023-09-07 10:45:27 +0800

    字典组件支持渲染为多选、多选下拉

--- src/components/DictData/index.vue
+++ src/components/DictData/index.vue
@@ -1,42 +1,54 @@
+<!-- 字典组件 -->
 <template>
   <template v-if="type === 'text'">
-    <template v-for="item in data">
-      <span v-if="modelValue == item.dictValue" :key="item.dictCode">
-        {{ item.dictLabel }}
-      </span>
-    </template>
+    <span v-for="item in valueData" :key="item.dictCode">
+      {{ item.dictLabel }}
+    </span>
   </template>
   <template v-else-if="type === 'tag'">
-    <template v-for="item in data">
-      <el-tag
-        v-if="modelValue == item.dictValue"
-        :key="item.dictCode"
-        size="small"
-        :type="item.listClass == 'primary' ? '' : item.listClass"
-        :disable-transitions="true"
-      >
-        {{ item.dictLabel }}
-      </el-tag>
-    </template>
+    <el-tag
+      v-for="item in valueData"
+      :key="item.dictCode"
+      :disable-transitions="true"
+      size="small"
+      :type="item.listClass == 'primary' ? '' : item.listClass"
+    >
+      {{ item.dictLabel }}
+    </el-tag>
   </template>
   <el-radio-group
     v-else-if="type === 'radio'"
     :disabled="disabled"
-    :model-value="modelValue"
     @update:modelValue="updateValue"
+    :model-value="modelValue"
   >
     <el-radio v-for="item in data" :key="item.dictCode" :label="item.dictValue">
       {{ item.dictLabel }}
     </el-radio>
   </el-radio-group>
+  <el-checkbox-group
+    v-else-if="type === 'checkbox'"
+    :disabled="disabled"
+    @update:modelValue="updateValue"
+    :model-value="modelValue"
+  >
+    <el-checkbox
+      v-for="item in data"
+      :key="item.dictCode"
+      :label="item.dictValue"
+    >
+      {{ item.dictLabel }}
+    </el-checkbox>
+  </el-checkbox-group>
   <el-select
     v-else
-    clearable
-    :disabled="disabled"
-    :model-value="modelValue"
-    :placeholder="placeholder"
-    class="ele-fluid"
     @update:modelValue="updateValue"
+    :model-value="modelValue"
+    :clearable="true"
+    :disabled="disabled"
+    :placeholder="placeholder"
+    :multiple="type === 'multipleSelect'"
+    class="ele-fluid"
   >
     <el-option
       v-for="item in data"
@@ -48,25 +60,45 @@
 </template>
 
 <script setup>
+  import { computed } from 'vue';
   import { useDictData } from '@/utils/use-dict-data';
 
   const emit = defineEmits(['update:modelValue']);
 
   const props = defineProps({
-    // 值
-    modelValue: [String, Number],
+    // 字典值
+    modelValue: [String, Number, Boolean, Array],
+    // 字典类型
+    code: String,
+    // 组件类型
+    type: String,
     // 是否禁用
     disabled: Boolean,
     // 提示文本
-    placeholder: String,
-    // 类型
-    type: String,
-    // 字典类型
-    code: String
+    placeholder: String
   });
 
   // 字典数据
   const [data] = useDictData([props.code]);
+
+  // 绑定值对应的字典数据
+  const valueData = computed(() => {
+    const result = [];
+    const val = props.modelValue;
+    if (val == null || val === '') {
+      return result;
+    }
+    const values = Array.isArray(val) ? val : [val];
+    values.forEach((v) => {
+      const temp = data.value.find((d) => d.dictValue == v);
+      if (temp != null) {
+        result.push(temp);
+      } else {
+        result.push({ dictCode: v, dictValue: v, dictLabel: v });
+      }
+    });
+    return result;
+  });
 
   /* 更新选中数据 */
   const updateValue = (value) => {
